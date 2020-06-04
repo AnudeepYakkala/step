e3d65f52035b1407e3b3a17ddfd7af510608f4e7
Anudeep <ayakkala@purdue.edu>
2020-06-04 19:51:32 +0000

    Fix javadoc indentation.

--- portfolio/src/main/java/com/google/sps/servlets/DeleteServerlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DeleteServerlet.java
@@ -12,9 +12,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
-  * Get comments data from Datastore. Delete all of the comments obtained
-  * from Datastore. Then, return an empty reponse. 
-  */
+ * Get comments data from Datastore. Delete all of the comments obtained
+ * from Datastore. Then, return an empty reponse.
+ */
 @WebServlet("/delete-comments")
 public class DeleteServerlet extends HttpServlet {
   @Override
