102bdf2a4a45e6acb21204f9e2ff8a6be3015c97
Anudeep <ayakkala@purdue.edu>
2020-06-04 06:11:23 +0000

    Fix Optional<String> syntax.

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -44,9 +44,9 @@ public class DataServlet extends HttpServlet {
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     // Get the comment input from the form.
-    Optional comment = getParameter(request, "text-input");
+    Optional<String> comment = getParameter(request, "text-input");
     if (comment.isPresent()) {
-      messages.add((String) comment.get());
+      messages.add(comment.get());
     }
 
     // Respond with the result.
@@ -73,6 +73,6 @@ public class DataServlet extends HttpServlet {
     if (value == null) {
       return Optional.empty();
     }
-    return Optional.of(value);
+    return Optional.ofNullable(value);
   }
 }
