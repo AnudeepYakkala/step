9e873db2df45a1668f2c23901890b7747ad6bb52
Anudeep <ayakkala@purdue.edu>
2020-06-08 21:53:06 +0000

    Fix styling issues.

--- portfolio/src/main/java/com/google/sps/servlets/LoginServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/LoginServlet.java
@@ -10,10 +10,11 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
-    
+  private static final String CONTENT_TYPE = "text/html;";
+
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html");
+    response.setContentType(CONTENT_TYPE);
 
     UserService userService = UserServiceFactory.getUserService();
     if (userService.isUserLoggedIn()) {
