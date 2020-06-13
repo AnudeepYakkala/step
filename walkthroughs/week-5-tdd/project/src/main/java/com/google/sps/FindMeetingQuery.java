81eb8edd75f9e57188f74c0312cf449acc702ed6
Anudeep <ayakkala@purdue.edu>
2020-06-13 00:28:16 +0000

    Implement algorithm to find possible times to schedule a meeting with no conflicts.

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,77 @@
 
 package com.google.sps;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 
 public final class FindMeetingQuery {
+  /*
+   * @return the Collection of TimeRanges containing all the possible time frames to schedule
+   * the request ensuring that the attandees don't have any conflicts with other events.
+   */
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    ArrayList<TimeRange> requestAttendeeRanges =
+        filterRequestAttendeeTimeRanges(events, request.getAttendees());
+    Collections.sort(requestAttendeeRanges, TimeRange.ORDER_BY_START);
+    requestAttendeeRanges = combineOverlaps(requestAttendeeRanges);
+    return findMeetingRangesWithNoConflict(requestAttendeeRanges, request.getDuration());
+  }
+
+  /*
+   * @return an ArrayList of all the events with at least one attendee from the request.
+   */
+  private ArrayList<TimeRange> filterRequestAttendeeTimeRanges(
+      Collection<Event> events, Collection<String> requestAttendees) {
+    ArrayList<TimeRange> requestAttendeeRanges = new ArrayList<>();
+    for (Event event : events) {
+      if (!Collections.disjoint(event.getAttendees(), requestAttendees)) {
+        requestAttendeeRanges.add(event.getWhen());
+      }
+    }
+    return requestAttendeeRanges;
+  }
+
+  /*
+   * @return an ArrayList with all the overlapping TimeRanges combined
+   */
+  private ArrayList<TimeRange> combineOverlaps(ArrayList<TimeRange> ranges) {
+    ArrayList<TimeRange> combinedRanges = new ArrayList<>();
+    for (int i = 0; i < ranges.size(); i++) {
+      TimeRange currentRange = ranges.get(i);
+      while (i + 1 < ranges.size() && currentRange.overlaps(ranges.get(i + 1))) {
+        // Combine the two overlapping TimeRanges into a single TimeRange
+        currentRange = TimeRange.fromStartEnd(currentRange.start(),
+            Math.max(currentRange.end(), ranges.get(i + 1).end()),
+            /* inclusive=*/false);
+        i++;
+      }
+      combinedRanges.add(currentRange);
+    }
+    return combinedRanges;
+  }
+
+  /*
+   * @return an ArrayList containing all the TimeRanges with no conflict that are long enough for
+   * the request.
+   */
+  private ArrayList<TimeRange> findMeetingRangesWithNoConflict(
+      ArrayList<TimeRange> ranges, long requestDuration) {
+    ArrayList<TimeRange> noConflictRanges = new ArrayList<>();
+    int currentStart = TimeRange.START_OF_DAY;
+    // Find all the ranges with no conflicts that are long enough for the request meeting.
+    for (TimeRange meetingRange : ranges) {
+      if (meetingRange.start() - currentStart >= requestDuration) {
+        noConflictRanges.add(
+            TimeRange.fromStartEnd(currentStart, meetingRange.start(), /* inclusive=*/false));
+      }
+      currentStart = meetingRange.end();
+    }
+    // Check if there is time left for the request between the last meeting and the end of the day.
+    if (TimeRange.END_OF_DAY - currentStart >= requestDuration) {
+      noConflictRanges.add(
+          TimeRange.fromStartEnd(currentStart, TimeRange.END_OF_DAY, /* inclusive=*/true));
+    }
+    return noConflictRanges;
   }
 }
